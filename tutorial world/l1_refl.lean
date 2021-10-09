1e68a6341616450c7f5d3e2b35a56fc95bdfced4
mon-neutrino <monica.sing.2023@gmail.com>
2021-10-09 22:16:31 +0700

    unfinished

--- tutorial world/l1_refl.lean
+++ tutorial world/l1_refl.lean
@@ -1,5 +1,11 @@
 namespace Nat
 
-theorem example1 (x y z : Nat) : x * y + z = x * y + z := 
+theorem example1 (x y z : Nat) : x * y + z = x * y + z :=
+  rfl
+
+theorem example2 (x y : Nat) (h : y = x + 7) : 2 * y = 2 * (x + 7) :=
+  rw
 
 end Nat
+
+
